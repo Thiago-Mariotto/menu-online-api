4b0b9108166500aaf40827991975f9b28a9ce354
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-09-30 21:28:54 -0300

    chore: implements Name class

---
+++ src/entities/valuesObject/Name.ts
@@ -0,0 +1,43 @@
+export default class Name {
+  private name: string;
+
+  private static minLength: number = 3;
+
+  private constructor(name: string) {
+    this.name = name;
+  }
+
+  public equals(name: Name): boolean {
+    return this.name === name.value;
+  }
+
+  public static fromString(name: string) {
+    if (!Name.isAValidName(name)) throw new Error('Invalid Name');
+    return new Name(name);
+  }
+
+  public static isAValidName(name: string): boolean {
+    if (!Name.hasValidLength(name)) return false;
+    if (Name.hasSpecialChars(name)) return false;
+    if (Name.hasNumbers(name)) return false;
+    return true;
+  }
+
+  private static hasValidLength(name: string): boolean {
+    return name.length >= Name.minLength;
+  }
+
+  private static hasSpecialChars(name: string): boolean {
+    const specialChars = /[`!@#$%^&*()_+\-=[\]{};':"\\|,.<>\\/?~]/;
+    return specialChars.test(name);
+  }
+
+  private static hasNumbers(name: string): boolean {
+    const numbers = /[0123456789]/;
+    return numbers.test(name);
+  }
+
+  public get value(): string {
+    return this.name;
+  }
+}
