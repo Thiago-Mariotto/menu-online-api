b56c0dce8b12657444ed53ff63df45e34c6159ef
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-10-04 12:11:34 -0300

    chore(types): implement user type

---
+++ src/types/User.ts
@@ -0,0 +1,10 @@
+export type UserRegisterDTO = {
+  cnpj: string;
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
+  cep: string;
+  number: string;
+  complement: string;
+}
