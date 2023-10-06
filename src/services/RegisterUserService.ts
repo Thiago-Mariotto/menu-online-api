1308cf96439e38aa7641c9bf75e8b4a5500c0873
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-10-05 21:42:20 -0300

    chore: rename file

---
+++ src/services/RegisterUserService.ts
@@ -0,0 +1,39 @@
+import User from '../entities/User';
+import UserBuilder from '../entities/UserBuilder';
+import { UserRegisterDTO } from '../types/User';
+import AddressService from './AddressService';
+import UserDataService from './UserDataService';
+
+export default class RegisterUserService {
+  private _user: User;
+
+  constructor(data: UserRegisterDTO) {
+    this._user = new UserBuilder(data.cnpj)
+      .setName(data.name)
+      .setEmail(data.email)
+      .setPassword(data.password)
+      .setPhone(data.phone)
+      .setCep(data.cep)
+      .setNumber(data.number)
+      .setComplement(data.complement)
+      .build();
+  }
+
+  public async execute() {
+    await this.validateUser(this._user);
+    const newUser = await UserDataService.saveUser(this._user);
+    this._user.id = newUser.userId;
+    await AddressService.checkUserAddress(this._user);
+    return;
+  }
+
+  private async validateUser(user: User) {
+    if (await UserDataService.getUserByCnpj(user.cnpj.value)) {
+      throw new Error('CNPJ já cadastrado');
+    }
+
+    if (await UserDataService.getUserByEmail(user.email.value)) {
+      throw new Error('Email já cadastrado');
+    }
+  }
+}
