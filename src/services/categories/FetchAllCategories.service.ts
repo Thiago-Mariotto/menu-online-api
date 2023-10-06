cb1dfdee3679c24a4e286a4b6b5d259813a48751
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-10-06 20:43:05 -0300

    refactor(model): rename model

--- src/services/categories/FetchAllCategories.service.ts
+++ src/services/categories/FetchAllCategories.service.ts
@@ -4,9 +4,9 @@ import { IService } from '../IService';
 
 export default class FetchAllCategoriesService implements IService<never, Categories> {
   private _prisma = new PrismaClient();
-  
+
   public async execute() {
-    const categories = await this._prisma.category.findMany();
+    const categories = await this._prisma.categoryModel.findMany();
     return categories;
   }
 }
