424120467811c27b079b9cbee35895ed5fbc166d
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-10-06 20:42:27 -0300

    refactor: remove reference to address

--- src/services/user/RegisterUser.service.ts
+++ src/services/user/RegisterUser.service.ts
@@ -1,30 +1,24 @@
+import { UserModel } from '@prisma/client';
 import User from '../../entities/User';
 import UserBuilder from '../../entities/UserBuilder';
 import { UserRegisterDTO } from '../../types/User';
-import AddressService from '../address/AddressService';
+import { IService } from '../IService';
 import UserDataService from './UserData.service';
 
-export default class RegisterUserService {
-  private _user: User;
+export default class RegisterUserService implements IService<UserRegisterDTO, UserModel>{
 
-  constructor(data: UserRegisterDTO) {
-    this._user = new UserBuilder(data.cpf)
+  public async execute(data: UserRegisterDTO) {
+    const user = new UserBuilder(data.cpf)
       .setName(data.name)
       .setEmail(data.email)
       .setPassword(data.password)
       .setPhone(data.phone)
       .build();
+    await this.validateUser(user);
+    return await UserDataService.saveUser(user);
   }
 
-  public async execute() {
-    await this.validateUser(this._user);
-    const newUser = await UserDataService.saveUser(this._user);
-    this._user.id = newUser.userId;
-    await AddressService.checkUserAddress(this._user);
-    return;
-  }
-
-  private async validateUser(user: User): Promise<void> {
+  private async validateUser(user: User) {
     if (await UserDataService.getUserByCPF(user.cpf.value)) {
       throw new Error('CPF já cadastrado');
     }
