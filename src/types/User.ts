ba3c112c27a120b1f1985e4a97b2b54bdde32328
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-10-05 23:08:39 -0300

    chore: user data input

--- src/types/User.ts
+++ src/types/User.ts
@@ -8,3 +8,15 @@ export type UserRegisterDTO = {
   number: string;
   complement: string;
 }
+
+export type TUserInputData = {
+  cnpj: string,
+  name: string,
+  email: string,
+  password: string,
+  role: 'admin' | 'user',
+  active: true | false,
+  lastLogin: Date,
+  createdAt: Date,
+  phone: string
+}
