e7d96b5a7deddb0ac6381a77bcdb59fa4f9a7f7f
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-10-10 12:18:18 -0300

    chore: client types

---
+++ src/types/Client.ts
@@ -0,0 +1,12 @@
+export type TCreationClientDTO = {
+  name: string;
+  email: string;
+  phone: string;
+}
+
+export type TCreatedClient = {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+}
