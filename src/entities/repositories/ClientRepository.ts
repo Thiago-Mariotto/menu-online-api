f4aa5b6f11421e7e66094cdead5ce23a4bb99794
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-10-11 10:42:18 -0300

    refator: change save method name

--- src/entities/repositories/ClientRepository.ts
+++ src/entities/repositories/ClientRepository.ts
@@ -2,9 +2,9 @@ import { TCreatedClient } from '../../types/Client';
 import Client from '../Client';
 
 interface IClientRepository {
-  create(data: Client): Promise<TCreatedClient>;
-  findByEmail(email: string): Promise<TCreatedClient>;
-  findById(id: string): Promise<TCreatedClient>;
+  save(data: Client): Promise<TCreatedClient>;
+  findByEmail(email: string): Promise<TCreatedClient | null>;
+  findById(id: string): Promise<TCreatedClient | null>;
 }
 
 export default IClientRepository;
