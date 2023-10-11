3532c650bb5f643ec0f340c216f1ca42ebc9c9a9
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-10-11 14:30:26 -0300

    feat: fetch client by id

--- tests/mocks/client.mock.ts
+++ tests/mocks/client.mock.ts
@@ -3,3 +3,9 @@ export const validClient = {
   phone: '123456789',
   email: 'joh@mail.com'
 }
+export const clientFromDb = {
+  clientId: '391f699f-b27a-403b-b8c4-4c7c94219b7d',
+  name: 'John Doe',
+  phone: '123456789',
+  email: 'joh@mail.com'
+}
