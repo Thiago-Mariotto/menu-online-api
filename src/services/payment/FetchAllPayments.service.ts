9aa1d4beb2d6cd65cc4a13ce700280ed15427025
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-10-13 16:21:13 -0300

    feat: service to fetch all payment methods

---
+++ src/services/payment/FetchAllPayments.service.ts
@@ -0,0 +1,16 @@
+import IPaymentRepository from '../../entities/repositories/PaymentRespository';
+import { TCreatedPayment } from '../../types/Payment';
+import { IService } from '../IService';
+
+export default class FetchAllPaymentsService implements IService<never, TCreatedPayment[]> {
+  private _paymentRepository: IPaymentRepository;
+
+  constructor(paymentRepository: IPaymentRepository) {
+    this._paymentRepository = paymentRepository;
+  }
+
+  public async execute() {
+    const payments = await this._paymentRepository.getAll();
+    return payments;
+  }
+}
