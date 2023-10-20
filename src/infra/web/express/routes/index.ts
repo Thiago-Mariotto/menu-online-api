6bd269b08b44257573d0515e13d76d1850c439e4
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-10-20 15:05:29 -0300

    chore: add routes

--- src/infra/web/express/routes/index.ts
+++ src/infra/web/express/routes/index.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import addressRouter from './address.route';
+import categoryRouter from './category.route';
 import clientRouter from './client.route';
+import paymentRouter from './payment.route';
 import userRouter from './user.route';
 
 const router = Router();
+
 router.use('/addresses', addressRouter);
 router.use('/users', userRouter);
 router.use('/clients', clientRouter);
+router.use('/categories', categoryRouter);
+router.use('/payments', paymentRouter);
 
 export default router;
