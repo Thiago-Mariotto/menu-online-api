7458da389aef451666e0cb985d62429c0945cc3d
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-10-20 15:05:53 -0300

    chore: add new payment method

--- src/repositories/payment/adapters/PaymentMemoryRepository.ts
+++ src/repositories/payment/adapters/PaymentMemoryRepository.ts
@@ -6,7 +6,12 @@ export default class PaymentMemoryRepository implements PaymentRepository {
 
   constructor() {
     this._table = [];
+    this._table.push({
+      paymentId: 'a83e17c0-d3b8-455b-832a-324e2bc5cdc3',
+      name: 'Cartão de Crédito'
+    });
   }
+
 
   public async getAll(): Promise<TCreatedPayment[] | []> {
     return this._table;
