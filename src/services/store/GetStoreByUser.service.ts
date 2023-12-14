2a3daa79b07817b2ea3f36bcb7e81f5db7450788
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-12-13 23:00:53 -0300

    refactor: validate credentials

--- src/services/store/GetStoreByUser.service.ts
+++ src/services/store/GetStoreByUser.service.ts
@@ -1,13 +1,18 @@
+import Forbidden from '../../errors/Forbidden';
 import IStoreRepository from '../../repositories/store/IStoreRepository';
-import { TLoggedInputDTO } from '../../types/Login';
-import { TStoreCreated } from '../../types/Store';
+import { TGetStoreByUserDTO, TStoreCreated } from '../../types/Store';
 import { IService } from '../IService';
 
-export default class GetStoreByUser implements IService<TLoggedInputDTO, TStoreCreated[]> {
+export default class GetStoreByUser implements IService<TGetStoreByUserDTO, TStoreCreated[]> {
   constructor(private _storeRepository: IStoreRepository) { }
 
-  async execute(loggedInput: TLoggedInputDTO): Promise<TStoreCreated[]> {
-    const userStores = await this._storeRepository.findByUserId(loggedInput.id);
+  async execute(data: TGetStoreByUserDTO): Promise<TStoreCreated[]> {
+    this._checkIsOwnerOrThrow(data.userId, data.session.id);
+    const userStores = await this._storeRepository.findByUserId(data.userId);
     return userStores;
+  }
+
+  private _checkIsOwnerOrThrow(userId: string, sessionUserId: string): void {
+    if (userId !== sessionUserId) throw new Forbidden('Você não tem permissão para acessar essas informações');
   }
 }
