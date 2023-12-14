d26c1b1eb0a6a4ba46d07a56b7c58304c3ef1b25
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-12-13 23:00:41 -0300

    refactor: validate credentials

--- src/services/store/GetStoreById.service.ts
+++ src/services/store/GetStoreById.service.ts
@@ -1,26 +1,25 @@
 import Forbidden from '../../errors/Forbidden';
 import NotFound from '../../errors/NotFound';
 import IStoreRepository from '../../repositories/store/IStoreRepository';
-import { TLoggedInputDTO } from '../../types/Login';
-import { TStoreCreated } from '../../types/Store';
+import { TGetStoreDTO, TStoreCreated } from '../../types/Store';
 import { IService } from '../IService';
 
 export default class GetStoreByIdService implements
-  IService<TLoggedInputDTO, Promise<TStoreCreated>> {
+  IService<TGetStoreDTO, Promise<TStoreCreated>> {
   constructor(
     private _storeRepository: IStoreRepository
   ) { }
 
-  public async execute(storeDTO: TLoggedInputDTO):
+  public async execute(data: TGetStoreDTO):
     Promise<Promise<TStoreCreated>> {
-    const store = await this._storeRepository.findById(storeDTO.id);
-
-    console.log('userId', storeDTO.loggedUser.id);
-    console.log('storeId', storeDTO.id);
-
+    const store = await this._storeRepository.findById(data.storeId);
     if (!store) throw new NotFound('Loja não encontrada');
-    if (store.userId !== storeDTO.loggedUser.id)
-      throw new Forbidden('Você não tem permissão para acessar essa loja');
+    this._checkIsOwnerOrThrow(store.userId, data.session.id);
     return store;
+  }
+
+  private _checkIsOwnerOrThrow(storeId: string, sessionUserId: string): void {
+    if (storeId !== sessionUserId)
+      throw new Forbidden('Você não tem permissão para acessar essa loja');
   }
 }
