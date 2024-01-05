fd1b27cf23990a58e98e049fd04f56d203f98067
Thiago Mariotto <thiagomariotto@outlook.com.br>
2024-01-05 14:32:17 -0300

    feat: implements order router

--- api_gateway/main.py
+++ api_gateway/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from src.routers import products
+from src.routers import orders
 
 app = FastAPI()
 
 
 app.include_router(products.router)
+app.include_router(orders.router)
 
 @app.get("/")
 async def index():
