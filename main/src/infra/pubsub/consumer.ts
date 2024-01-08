e21866d3db4ad380e9bf1e097d323f36e39d732a
Thiago Mariotto <thiagomariotto@outlook.com.br>
2024-01-08 17:19:13 -0300

    feat: implements transaction withdatabase

--- main/src/infra/pubsub/consumer.ts
+++ main/src/infra/pubsub/consumer.ts
@@ -1,9 +1,12 @@
+/* eslint-disable indent */
 import EventEmitter from 'events';
 import Kafka from 'node-rdkafka';
+import ProductPrismaRepository from '../../repositories/product/adapter/ProductPrismaRepository';
+import { TProductCreated } from '../../types/Product';
 
 const pubSubEventManager = new EventEmitter();
 
-let consumer : Kafka.KafkaConsumer;
+let consumer: Kafka.KafkaConsumer;
 
 pubSubEventManager.on('start-consumer', (topics: string[]) => {
   console.log('Iniciando consumer do Kafka');
@@ -14,23 +17,49 @@ pubSubEventManager.on('start-consumer', (topics: string[]) => {
 
   consumer.connect();
 
-  consumer
-    .on('ready', () => {
-      console.log('calling consume');
-      consumer.subscribe(topics);
-      consumer.consume();
-    })
-    .on('data', (data): void => {
-      if (!data.value) return;
-      switch (data.topic) {
-        case 'orders':    
-            console.log(data.value.toString());
-        default:
-          return;
-      }
-    });
+  consumer.on('ready', () => {
+    consumer.subscribe(topics);
+    consumer.consume();
+  })
+    .on('data', handleMessage);
 
 });
+
+const handleMessage = async (data: any): Promise<void> => {
+  if (!data.value) return;
+  switch (data.topic) {
+    case 'orders': {
+      const parsedData = JSON.parse(data.value.toString());
+      await updateProduct(parsedData.products);
+      break;
+    }
+    default:
+      return;
+  }
+};
+
+type ProductDTO = {
+  product_id: string;
+  quantity: number;
+}
+
+const updateProduct = async (products: ProductDTO[]) => {
+  const prodPrismaRepo = new ProductPrismaRepository();
+  const findedProducts = products.map((product: ProductDTO) => {
+    return prodPrismaRepo.getById(product.product_id);
+  });
+
+  const productsToUpdate = await Promise.all(findedProducts);
+
+  productsToUpdate.forEach(async (product: TProductCreated | null) => {
+    if (!product) return;
+    const productInd = products.findIndex((prod) => prod.product_id === product.productId);
+    console.log('alterando produto', product.productId);
+    const newQuantity = product.quantity - products[productInd].quantity;
+    await prodPrismaRepo.setProductQuantity(product.productId, newQuantity);
+  });
+
+};
 
 pubSubEventManager.on('stop-consumer', () => {
   console.log('Parando consumer do Kafka');
