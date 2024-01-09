638b40fae7f4871695f238725e11b86b331e4596
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2024-01-09 16:44:42 -0300

    feat: populate redis on start

--- api_gateway/main.py
+++ api_gateway/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from src.routers import products
 from src.routers import orders
+import redis
 
 app = FastAPI()
 
@@ -10,3 +11,14 @@ app.include_router(orders.router)
 @app.get("/")
 async def index():
   return {"message": "Welcome to api gateway!"}
+
+
+try:
+  print('start')
+  redisConnection = redis.Redis(host='redis', port=6379, decode_responses=True)
+  redisConnection.set('a014dcb7-a36d-4376-97d2-f319fe14690e', '50')
+  print('populando')
+except:
+  print('## Erro ao connection ao Redis')
+finally:
+  redisConnection.close()
